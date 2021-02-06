be3700b77c12b719c0b10f0a72a39bf855c3a249
hamdy <razereng0@gmail.com>
2021-02-06 20:23:50 +0200

    done

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app)
 const io = require('socket.io')(server, {
-    // cors: {
-    //     origin: process.env.clientServer
-    // }
+    cors: {
+        origin: process.env.clientServer
+    }
+})
+
+io.on('connection', socket => {
+    console.log('connected to socket')
 })
 const productRoute = require('./routes/products.route');
 const authRoute = require('./routes/auth.route');
